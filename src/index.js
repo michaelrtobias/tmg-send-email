862ae0107ae90e152ae091cc5bef193a4682cb87
Michael Tobias <michaelremitobias@gmail.com>
2022-02-09 20:22:25 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ exports.handler = function ({ args: data }) {
     console.log("results:", results);
     console.log("results first name:", results.first_name);
     console.log("results last name:", results.last_name);
-    console.log("results email:", results.email);
+
     return results;
   } catch (e) {
     throw e;
