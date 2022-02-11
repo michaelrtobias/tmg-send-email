b6a495d8a18fa711c44e2cd234e1b0363a95c7e3
Michael Tobias <michaelremitobias@gmail.com>
2022-02-10 18:36:08 -0700

    4

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ exports.handler = function ({ args: data }) {
   try {
     console.log("results first name:", results.first_name);
     console.log("results product type:", results.type);
-    console.log(4);
+    console.log(5);
   } catch (e) {
     throw e;
   }
