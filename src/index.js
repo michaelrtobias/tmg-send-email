03a755a319a01145aa8806a65a332ef34f2d0b0b
Michael Tobias <michaelremitobias@gmail.com>
2022-02-10 17:51:04 -0700

    changes again

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ const AWS = require("aws-sdk");
 exports.handler = function ({ args: data }) {
   let results = data.contactInfo;
   try {
-    console.log("results:", results);
     console.log("results first name:", results.first_name);
     console.log("results product type:", results.type);
-    console.log("results product brand", results.make);
   } catch (e) {
     throw e;
   }
